fae8958d882804d2605b8ebc5542030838b2624a
Amit Kumar <Amit.Kumar338@T-Mobile.com>
2022-02-07 18:39:05 +0530

    Heroku Scripts added

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const path = require("path");
 const contactRouter = require("./routes/contactRoute");
 
 //applying middleware to make code a bit cleaner
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.use("/", contactRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 // set port, listen for requests
 const PORT = process.env.PORT || 5000;
